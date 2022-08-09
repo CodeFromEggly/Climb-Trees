bf90a4c8b6c5d4a04f734afde55f4c70434f7497
CodeFromEggly <108484834+CodeFromEggly@users.noreply.github.com>
2022-08-09 13:40:59 +0100

    Create AddTree.py

---
+++ AddTree.py
@@ -0,0 +1,36 @@
+# H Wells 08/2022
+# This script will update the file 'tree_database.csv', based upon user input.
+
+# When ran, ask for: latitude, longitude, difficulty, name
+
+print("Okay - Let's add a tree\n")
+
+
+# Lat & long:
+
+# Initially, let's assume input is only in decimal degrees.
+latitude = input("Please enter latitude [DD]:   ")
+
+longitude = input("and enter longitude [DD]:    ")
+
+
+# Difficulty will be added as a number, the code will append 'T'
+difficulty = input("Please enter a numerical rank for said tree:    ")
+difficulty = "".join(["T",difficulty])
+
+print("You're adding a " + difficulty + " tree at: " + latitude + " lat, ", longitude, " long.")
+
+name = input("Add a custom name? [y/n]  ")
+if (name.lower() == 'y') :
+    name = input("Enter the custom name:    ")
+else :
+    # A default name for now. In the future it could be named Tree X where X is its number in the database
+    name = "Tree"
+
+
+
+# Can now update the CSV file
+
+import csv
+
+filename = 'test_database.csv'
