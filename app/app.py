6cd977325829602b999c1b49e0e88d83efe04b02
CodeFromEggly <108484834+CodeFromEggly@users.noreply.github.com>
2022-12-31 17:50:38 +0000

    Updated /templates/, app.py and SQL
    Adding in functionality for creating users accounts.
    Taskbar for map in /location now done with Jinja loop.
    Stuck on SQL for /register at the moment

--- app/app.py
+++ app/app.py
@@ -7,6 +7,12 @@ from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
+from helpers import login_required
+
+from datetime import datetime
+
+
+
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
@@ -18,7 +24,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
-
+# CS50 had this... seems important
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -26,6 +32,7 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
 
 @app.route("/")
 def index():
@@ -35,7 +42,7 @@ def index():
 
 
 @app.route("/location", methods=["GET", "POST"])
-def myLocation():
+def location():
     # New conditions entered. Change .json and redirect to index
     if request.method == "POST":
         """ This will execute when a POST (likely NavBar button or form) is submitted to the /location route"""
@@ -48,6 +55,136 @@ def myLocation():
         return render_template("location.html")
 
 
+""" Requets for a tree creation (can be done from location.html for example) """
+@app.route("/createTree", methods=["GET", "POST"])
+@login_required
+def createTree():
+
+    if request.method == "POST":
+
+        return redirect("/")
+    
+    else:
+        return render_template("newTree.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+    #SQLite3 query to detect listings.
+    conn = sqlite3.connect('treeHub.db')
+    conn.row_factory = sqlite3.Row
+    db = conn.cursor()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            # TODO error: return apology("must provide username", 403)
+            print("no username")
+            return redirect("/login")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            # TODO error: return apology("must provide password", 403)
+            print("no password")
+            return redirect("/login")
+
+        # Query database for username
+        all = db.execute("SELECT * FROM users WHERE username = ?", username).fetchall()
+        for row in all: row = dict(row)
+
+        # TODO Ensure username exists and password is correct
+        if len(row) != 1 or not check_password_hash(row[0]["hash"], password):
+            print("invalid username and/or password")
+            return redirect("/login")
+
+        
+        # Remember which user has logged in
+        #session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    #SQLite3 query to detect listings.
+    conn = sqlite3.connect('treeHub.db')
+    conn.row_factory = sqlite3.Row
+    db = conn.cursor()
+
+    # May as well clear the session
+    session.clear()
+
+    # Registration submitted via form:
+    if request.method == "POST":
+        
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        # Ensure username was submitted
+        if not username:
+            #TODO: error message
+            print("No Username")
+            return redirect("/register")
+
+        # Ensure password was submitted
+        elif not password:
+            #TODO: error message
+            print("no Password")
+            return redirect("/register")
+
+        # Ensure password confirmed
+        elif not (confirmation == password):
+            #TODO: error message
+            print("unconfirmed password")
+            return redirect("/register")
+
+        
+        # Ensure username doesn't exist already:
+        rows = db.execute("SELECT * FROM users WHERE username = ?", (username)).fetchall()
+        rows = [dict(row) for row in rows]
+        if len(rows) > 0: #TODO should it be 0?
+            # TODO: error message
+            print("username taken")
+            return redirect("/register")
+
+
+        # Create a new user in database:
+        hashword = generate_password_hash(request.form.get("password"))
+        # TODO: Add a user entry in sql:
+        print(f"inserting into users: {username} and {hashword}")
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", (username, hashword))
+
+        return redirect("/login")
+
+    # Or did they click a link to get here? ("GET" method)
+    else:
+        # They want to register
+        return render_template("register.html")
 
 if __name__ == "__main__":
     app.run()
--- app/static/minimap.js
+++ app/static/minimap.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([53.8, -0.4875], 13);
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
         maxZoom: 18
