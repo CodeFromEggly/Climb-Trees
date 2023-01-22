fbe0a31fc971ddd16511102cdacaa85524b37e6a
CodeFromEggly <108484834+CodeFromEggly@users.noreply.github.com>
2023-01-22 19:13:29 +0000

    MARKER CLUSTERS
    They work....
    
    npm modules updated too, it seems.
    xminimap.js is the old file

--- app/static/scripts/minimap.js
+++ app/static/scripts/minimap.js
@@ -1,71 +1,43 @@
-
-// Tests functionality using <p id="test"> in location.html
-var x = document.getElementById("test");
-x.innerHTML="Testing: ";
-
-// TODO Process trees into a list called treeList:
-treeList = []
-
-// Add each tree as a new marker to treeList
-let jinjatrees = trees
-for (let tree of jinjatrees) {
-  // Convert latitude and longitude into coords object {lat:x, lng: y}
-  var coords = {lat:tree.latitude, lng:tree.longitude};
-  var newMarker = {
-    position: coords,
-    id: tree.id,
-    grade: tree.grade,
-    planter: tree.planter
-  };
-  treeList.push(newMarker);
-}
+let jinjatrees = trees;
 
 
 
-// Create the map
-function initMap(){
-  // Set options for the map
-  var options = {
-    center:{lat:53.6490, lng:-1.7842},
-    zoom:4,
-    // TODO centre on device location (google has an API for this)
-    // Sets map type to Hybrid by default
-    mapTypeId: google.maps.MapTypeId.HYBRID
-  };
 
-  var map = new
-  google.maps.Map(document.getElementById('map'), options);
- 
-
-  const markers = treeList.coords.map((position, i) => {
-    const marker = new google.maps.Marker({
-      position
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: { lat: 53.8059209, lng: -1.6758146 },
     });
-
-    // markers can only be keyboard focusable when they have click listeners
-    // open info window when marker is clicked
-    marker.addListener("click", () => {
-      infoWindow.open(map, marker);
+    const infoWindow = new google.maps.InfoWindow({
+      content: "",
+      disableAutoPan: true,
     });
-    return marker;
-  });
+    
+    markers = [];
+    for (let tree of jinjatrees) {
+      // Convert latitude and longitude into coords object {lat:x, lng: y}
+      var coords = {lat:tree.latitude, lng:tree.longitude};
+    
+      var mk = new google.maps.Marker({
+        position: coords,
+        //Sets pins to drop onto screen animation
+        animation:google.maps.Animation.DROP,
+        // Icon set to "'PATH/tree' + '(GRADE)' + '.png'".   Doesn't seem to be case-sensitive
+        icon: "static/TreeIcons/tree".concat(tree.grade,".png"),
+        map,
+        // TODO information like grade and who 'planted' it
+        title: "Hello World!"
+      });
+      markers.push(mk);
+    }
+    
 
-  new MarkerClusterer({ markers, map });
-
-  // Loop through treeList adding each one
-  for (let i = 0, len = treeList.length; i < len; i++) {
-    new google.maps.Marker({
-      position: treeList[i].position,
-      //Sets pins to drop onto screen animation
-      animation:google.maps.Animation.DROP,
-      // Icon set to "'PATH/tree' + '(GRADE)' + '.png'".   Doesn't seem to be case-sensitive
-      icon: "static/TreeIcons/tree".concat(treeList[i].grade,".png"),
-      map,
-      // TODO information like grade and who 'planted' it
-      title: "Hello World!"
-    });
-
+    
+  
+    // Add a marker clusterer to manage the markers.
+    new markerClusterer.MarkerClusterer({ markers, map });
   }
+  
 
-}
-
+  
+  window.initMap = initMap;
