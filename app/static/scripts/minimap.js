11c9a737c7290bfc9bb92d0b6709bac8b5ee9d01
CodeFromEggly <108484834+CodeFromEggly@users.noreply.github.com>
2023-01-23 17:55:09 +0000

    Update minimap.js
    Added centre to geolocation.
    fixed indentation.
    
    Co-Authored-By: Trueelement123 <73363837+Trueelement123@users.noreply.github.com>

--- app/static/scripts/minimap.js
+++ app/static/scripts/minimap.js
@@ -4,46 +4,57 @@ let trees = jinjatrees;
 
 
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: { lat: 53.8059209, lng: -1.6758146 },
-      // TODO centre on device location (google has an API for this)7
-    
-      // Sets map type to Hybrid by default
-      mapTypeId: google.maps.MapTypeId.HYBRID
-    });
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: { lat: 53.8059209, lng: -1.6758146 },
+    // TODO centre on device location (google has an API for this)7
+  
+    // Sets map type to Hybrid by default
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  });
 
-    const infoWindow = new google.maps.InfoWindow({
-      content: "",
-      disableAutoPan: true,
-    });
-    
-    markers = [];
-    for (let tree of trees) {
-      // Convert latitude and longitude into coords object {lat:x, lng: y}
-      var coords = {lat:tree.latitude, lng:tree.longitude};
-    
-      var mk = new google.maps.Marker({
-        position: coords,
-        id: tree.id,
-        grade: tree.grade,
-        planter: tree.planter,
-        icon: "static/TreeIcons/tree".concat(tree.grade,".png"),
+  const infoWindow = new google.maps.InfoWindow({
+    content: "",
+    disableAutoPan: true,
+  });
 
-        //Sets pins to drop onto screen animation
-        animation:google.maps.Animation.DROP,
-        
-        map,
-        title: `Tree  ${tree.id}!`
-      });
-      markers.push(mk);
-    }
+  // If device allows access to location, centre map on its position.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        map.setCenter(initialLocation);
+        map.setZoom(4)
+    });
+  }
+    
+  markers = [];
+  for (let tree of trees) {
+    // Convert latitude and longitude into coords object {lat:x, lng: y}
+    var coords = {lat:tree.latitude, lng:tree.longitude};
+  
+    var mk = new google.maps.Marker({
+      position: coords,
+      id: tree.id,
+      grade: tree.grade,
+      planter: tree.planter,
+      icon: "static/TreeIcons/tree".concat(tree.grade,".png"),
+
+      //Sets pins to drop onto screen animation
+      animation:google.maps.Animation.DROP,
+      
+      map,
+      title: `Tree  ${tree.id}!`
+    });
+    markers.push(mk);
+  }
     
 
     
   
-    // Add a marker clusterer to manage the markers.
-    new markerClusterer.MarkerClusterer({ markers, map });
+  // Add a marker clusterer to manage the markers.
+  new markerClusterer.MarkerClusterer({ markers, map });
+
+  
   }
   
 
